41ed547cee435f33bede5c87ec990c7f3d963f06
VB <vb@Apple-MacBook-Pro.local>
2017-03-16 20:31:19 +0300

    Data show screen
    Firebase datamızdaki verileri cihazımızda liste şeklinde gösterme

---
+++ app/src/main/java/com/example/vb/firebaseblogapp/Blog.java
@@ -0,0 +1,43 @@
+package com.example.vb.firebaseblogapp;
+
+/**
+ * Created by vb on 16.03.2017.
+ */
+
+public class Blog {
+
+    String title,desc,image;
+
+    public Blog() {
+    }
+
+    public Blog(String title, String desc, String image) {
+        this.title = title;
+        this.desc = desc;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
--- app/src/main/java/com/example/vb/firebaseblogapp/MainActivity.java
+++ app/src/main/java/com/example/vb/firebaseblogapp/MainActivity.java
@@ -1,20 +1,91 @@
 package com.example.vb.firebaseblogapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView blog_list;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        blog_list= (RecyclerView) findViewById(R.id.rcView);
+        blog_list.setHasFixedSize(true);
+        blog_list.setLayoutManager(new LinearLayoutManager(this));
+        mDatabase= FirebaseDatabase.getInstance().getReference().child("Blog");
+
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Blog,BlogViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Blog, BlogViewHolder>
+                (
+                        Blog.class,
+                        R.layout.blog_row,
+                        BlogViewHolder.class,
+                        mDatabase
+
+                ) {
+            @Override
+            protected void populateViewHolder(BlogViewHolder viewHolder, Blog model, int position) {
+
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setDesc(model.getDesc());
+                viewHolder.setImage(model.getImage(),getApplicationContext());
+            }
+        };
+
+        blog_list.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+    public static class BlogViewHolder extends RecyclerView.ViewHolder{
+        View mview;
+
+        public BlogViewHolder(View itemView) {
+            super(itemView);
+            mview=itemView;
+        }
+
+        public void setTitle(String title){
+            TextView post_title = (TextView) mview.findViewById(R.id.post_title);
+            post_title.setText(title);
+
+        }
+        public void setDesc(String desc){
+            TextView post_desc = (TextView) mview.findViewById(R.id.post_desc);
+            post_desc.setText(desc);
+
+        }
+        public  void  setImage(String image, Context ctx){
+            ImageView post_image = (ImageView) mview.findViewById(R.id.post_image);
+            Picasso.with(ctx).load(image).into(post_image);
+        }
+
+    }
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
         //kendi yazdığımız menuyu projeye dahil ediyoruz
         getMenuInflater().inflate(R.menu.main_menu,menu);
